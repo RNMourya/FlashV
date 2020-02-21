23f3e30b2653f8bbebdd36ef4ac769bca044a3fa
SrikarAmara <srikaramara345@gmail.com>
2020-02-21 07:14:31 +0530

    Background_img_updated

--- app/src/main/java/com/example/flashv/MainActivity.java
+++ app/src/main/java/com/example/flashv/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.flashv;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ImageView imageView =  findViewById(R.id.image);
+        Glide.with(this).load(R.drawable.start).into(imageView);
 
        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
